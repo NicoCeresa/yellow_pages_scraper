b17061de0901ba5034d911edf697f9e1680bc0e0
Domenico Ceresa <82683503+NicoCeresa@users.noreply.github.com>
2023-09-20 09:08:40 -0700

    changed to method chaining

--- transform.py
+++ transform.py
@@ -17,10 +17,17 @@ class Transform:
         for column in df.columns:
             df[column] = df[column].apply(ast.literal_eval).str[0]
 
-        df['ratings'] = df['ratings'].apply(Transform.safe_literal_eval)
-        df['stars'] = df['ratings'].str["rating"]
-        df['num_reviews'] = df['ratings'].str["count"]
-        df['years_open'] = df['years_open'].fillna(0).astype(int)
+        df.pipe(lambda d: d.assign(
+            ratings = d.ratings.apply(Transform.safe_literal_eval),
+            stars = d.ratings.str['rating'],
+            num_reviews = d.ratings.str['count'],
+            years_open = d.years_open.fillna(0).astype(int),
+        ))
+
+        # df['ratings'] = df['ratings'].apply(Transform.safe_literal_eval)
+        # df['stars'] = df['ratings'].str["rating"]
+        # df['num_reviews'] = df['ratings'].str["count"]
+        # df['years_open'] = df['years_open'].fillna(0).astype(int)
         df['stars'] = df['stars'].fillna(0.0).replace("",0.0).astype(float)
         df['num_reviews'] = df['num_reviews'].fillna(0).replace("",0).astype(int)
         
