7a3e5f1044ee5d8d6032b8b33fc120721b581b91
Domenico Ceresa <82683503+NicoCeresa@users.noreply.github.com>
2023-09-20 09:09:59 -0700

    forgor to delete commented out redundant code

--- transform.py
+++ transform.py
@@ -24,10 +24,6 @@ class Transform:
             years_open = d.years_open.fillna(0).astype(int),
         ))
 
-        # df['ratings'] = df['ratings'].apply(Transform.safe_literal_eval)
-        # df['stars'] = df['ratings'].str["rating"]
-        # df['num_reviews'] = df['ratings'].str["count"]
-        # df['years_open'] = df['years_open'].fillna(0).astype(int)
         df['stars'] = df['stars'].fillna(0.0).replace("",0.0).astype(float)
         df['num_reviews'] = df['num_reviews'].fillna(0).replace("",0).astype(int)
         
